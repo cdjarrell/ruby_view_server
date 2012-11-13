0407d6164ccfbaff995e85e261d537b4047c355c
Cory Jarrell <cdjarrell@gmail.com>
2012-11-13 14:48:15 -0800

    homework section 1 completed

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,10 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+y = 48
+template = ERB.new "The value of z is: <%= y-x %>"
+puts template.result(binding)
+
+namesA = ['adam','alex','abe']
+namesA.each do |name| puts "Names with A are: #{name}" end
